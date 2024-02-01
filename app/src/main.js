c7c5e57f018fd4321604106b8d246b07e9f02d80
ismaelnrqz <ismaelnrqz@gmail.com>
2024-02-01 18:06:32 -0500

    i forgot to push

--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,25 @@
-import './style.css'
+import './style.css';
+
+const main = () => {
+  const hideOverlay = () => {
+    const delay = 4000;
+  
+    setTimeout(() => {
+      const overlay = document.getElementById('overlay');
+      const app = document.getElementById('app');
+      
+      overlay.style.opacity = 0;
+
+      app.style.display = 'block';
+      app.style.opacity = 1;
+    }, delay);
+  };
+
+  const backgroundMusic = document.getElementById('backgroundMusic');
+
+  backgroundMusic.play();
+
+  hideOverlay();
+};
+
+main();
